51a84ae423e72a1113b4493fd9f2b4c56a6a178c
lect-ad <64366181+lect-ad@users.noreply.github.com>
2024-06-04 22:02:07 +0200

    Update zenas_app.py

--- zenas_app.py
+++ zenas_app.py
@@ -1,4 +1,12 @@
 # Import python packages
 import streamlit as st
-import snowflake.connector
+import snowflake.connector as con
 import pandas as pd
+
+
+my_cnx = con.connect(**st.secrets["snowflake"]) 
+my_cur = my_cnx.cursor() 
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") 
+my_data_row = my_cur.fetchone() 
+st.text("Hello from Snowflake:") 
+st.text(my_data_row)
